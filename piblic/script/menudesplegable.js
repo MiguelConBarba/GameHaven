ef6e2a4257e564722b095d96c77f39cf820b5c37
Julianperezortiz2301 <perezjulian88@gmail.com>
2024-02-25 16:02:22 -0500

    (add) advance sidevar

--- piblic/script/menudesplegable.js
+++ piblic/script/menudesplegable.js
@@ -2,6 +2,21 @@
 const cloud = document.getElementById("cloud");
 const barralateral = document.querySelector(".barra-lateral")
 const spans = document.querySelectorAll("span");
+const palanca = document.querySelector(".switch");
+const circulo = document.querySelector(".circulo");
+const menu = document.querySelector(".menu");
+
+menu.addEventListener("click",()=>{
+    barralateral.classList.toggle
+});
+
+
+palanca.addEventListener("click",()=>{
+    let body = document.body;
+    body.classList.toggle("dark-mode");
+    circulo.classList.toggle("prendido");
+
+});
 
 cloud.addEventListener("click",()=>{
     barralateral.classList.toggle("mini-barra-lateral");
