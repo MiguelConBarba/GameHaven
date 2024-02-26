34d1cce24c897f1f196b9897181c653ac514d414
Julianperezortiz2301 <perezjulian88@gmail.com>
2024-02-26 15:54:49 -0500

    (update) fuctions js and css in the navbar

--- piblic/script/menudesplegable.js
+++ piblic/script/menudesplegable.js
@@ -5,9 +5,31 @@ const spans = document.querySelectorAll("span");
 const palanca = document.querySelector(".switch");
 const circulo = document.querySelector(".circulo");
 const menu = document.querySelector(".menu");
+const main = document.querySelector("main");
+
+main
 
 menu.addEventListener("click",()=>{
-    barralateral.classList.toggle
+    barralateral.classList.toggle("max-barra-lateral");
+    if(barralateral.classList.contains("max-barra-lateral")){
+        menu.children[0].style.display ="none";
+        menu.children[1].style.display ="block";
+    }
+
+    else{
+
+        menu.children[0].style.display ="block";
+        menu.children[1].style.display ="none";
+
+    }
+
+    if(window.innerWidth<=320){
+        barralateral.classList.add("mini-barra-lateral");
+        main.classList.add("min-main");
+        spans.forEach((span)=>{
+            span.classList.add("oculto");
+        })
+    }
 });
 
 
@@ -20,7 +42,7 @@ palanca.addEventListener("click",()=>{
 
 cloud.addEventListener("click",()=>{
     barralateral.classList.toggle("mini-barra-lateral");
-    
+    main.classList.toggle("min-main");
     spans.forEach((span)=>{
         span.classList.toggle("oculto");
     });
