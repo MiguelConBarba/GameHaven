74514e31be8ff43dcc5512626e8c28bfacbf4b41
Julianperezortiz2301 <perezjulian88@gmail.com>
2024-09-14 07:53:04 -0500

    (add) comienzo de barra de busqueda

---
+++ piblic/script/input-search.js
@@ -0,0 +1,32 @@
+document.getElementById('search-input').addEventListener('input', function () {
+    const searchQuery = this.value.toLowerCase();
+    const catalogItems = document.querySelectorAll('.mostrador .item');
+
+    catalogItems.forEach(item => {
+        const itemName = item.querySelector('.descripcion').textContent.toLowerCase();
+        if (itemName.includes(searchQuery)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
+
+
+document.getElementById('search-input').addEventListener('input', function () {
+    const searchQuery = this.value.toLowerCase();
+    const catalogItems = document.querySelectorAll('.item');
+    let hasResults = false;
+
+    catalogItems.forEach(item => {
+        const itemName = item.querySelector('p').textContent.toLowerCase();
+        if (itemName.includes(searchQuery)) {
+            item.style.display = 'block';
+            hasResults = true;
+        } else {
+            item.style.display = 'none';
+        }
+    });
+
+    document.getElementById('no-results').style.display = hasResults ? 'none' : 'block';
+});
