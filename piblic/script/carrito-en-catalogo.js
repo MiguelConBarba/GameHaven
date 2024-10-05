3ab478c407e79f032b007445971d2f81791ed1e5
AndresSg0716 <jandressg30@gmail.com>
2024-10-05 12:14:32 -0500

    (ADD & DELETE) header con carrito y incio de sesion y se elimino la anterior hoja de estilos del carrito

--- piblic/script/carrito-en-catalogo.js
+++ piblic/script/carrito-en-catalogo.js
@@ -1,23 +1,6 @@
-const btnCart = document.querySelector(".container-icons");
-const conteinerCartProducts = document.querySelector('.container-cart-products-cerrado');
-const btnCartClose = document.querySelector('.icon-close');
-const logoMenu = document.querySelector('#logo_menu');  
+const BtnCart = document.querySelector('.container-icon')
+const ContainerCartProductos = document.querySelector('.container-cart-products')
 
-conteinerCartProducts.classList.add('container-cart-products');
-
-btnCart.addEventListener('click', () => {
-    conteinerCartProducts.classList.toggle('icon-cart');
+BtnCart.addEventListener('click', () => {
+    ContainerCartProductos.classList.toggle('hidden-cart')
 })
-btnCartClose.addEventListener('click', () => {
-    conteinerCartProducts.classList.toggle('icon-cart');
-});
-
-logoMenu.addEventListener('click', () => {
-    if (conteinerCartProducts.classList.contains('container-cart-products')) {
-        conteinerCartProducts.classList.remove('container-cart-products');
-        conteinerCartProducts.classList.add('container-cart-products-cerrado');
-    } else {
-        conteinerCartProducts.classList.remove('container-cart-products-cerrado');
-        conteinerCartProducts.classList.add('container-cart-products');
-    }
-});
